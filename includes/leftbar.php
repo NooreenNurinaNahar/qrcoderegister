4b07da98c55555b02d5ac43c31bb61ef7ba9a5db
NooreenNurinaNahar <95007442+NooreenNurinaNahar@users.noreply.github.com>
2021-12-02 00:59:21 +0800

    Add files via upload

---
+++ includes/leftbar.php
@@ -0,0 +1,37 @@
+<div class="left-sidebar bg-black-300 box-shadow ">
+                        <div class="sidebar-content">
+                            <div class="user-info closed">
+                                <img src="http://placehold.it/90/c2c2c2?text=User" alt="John Doe" class="img-circle profile-img">
+                                <h6 class="title">ADMIN</h6>
+                                <small class="info">PHP Developer</small>
+                            </div>
+                            <!-- /.user-info -->
+
+                            <div class="sidebar-nav">
+                                <ul class="side-nav color-gray">
+                                    <li class="nav-header">
+                                        <span class="">Menu Utama</span>
+                                    </li>
+                                    <li>
+                                        <a href="dashboard2.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span> </a>
+                                     
+                                    </li>
+
+                                    <li class="nav-header">
+                                        <span class="">Pameran</span>
+                                    </li>
+
+                                    <li class="has-children">
+                                        <a href="#"><i class="fa fa-file-text"></i> <span>User</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="senarai_user.php"><i class="fa fa-bars"></i> <span>Senarai User</span></a></li>
+                                           
+                                           </li>
+                                        </ul>
+                                    </li>
+                                    
+                            </div>
+                            <!-- /.sidebar-nav -->
+                        </div>
+                        <!-- /.sidebar-content -->
+                    </div>
